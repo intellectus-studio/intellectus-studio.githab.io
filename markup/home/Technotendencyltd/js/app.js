f4ba2ecd487b4691b7e148f29611da3db691afb4
Olha Hryhorenko <olha.hryhorenko@i.ua>
2016-04-25 14:50:19 +0300

    Added files via upload

---
+++ markup/home/Technotendencyltd/js/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$('.news_head1').click(function() {
+		$('.news_description1').toggleClass('news_short');
+	});
+	$('.news_img1').click(function() {
+		$('.news_description1').toggleClass('news_short');
+	});
+	$('.news_head2').click(function() {
+		$('.news_description2').toggleClass('news_short');
+	});
+	$('.news_img2').click(function() {
+		$('.news_description2').toggleClass('news_short');
+	});
+	$('.news_head3').click(function() {
+		$('.news_description3').toggleClass('news_short');
+	});
+	$('.news_img3').click(function() {
+		$('.news_description3').toggleClass('news_short');
+	});
+	$('.news_head4').click(function() {
+		$('.news_description4').toggleClass('news_short');
+	});
+	$('.news_img4').click(function() {
+		$('.news_description4').toggleClass('news_short');
+	});
+	$('.news_head5').click(function() {
+		$('.news_description5').toggleClass('news_short');
+	});
+	$('.news_img5').click(function() {
+		$('.news_description5').toggleClass('news_short');
+	});
+})
