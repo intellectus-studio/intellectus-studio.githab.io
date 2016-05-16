5f870e313ea80c4e02011a3705030d85af67dc1d
Olha Hryhorenko <olha.hryhorenko@i.ua>
2016-05-16 14:58:34 +0300

    Add files via upload

--- site/js/script.js
+++ site/js/script.js
@@ -3749,3 +3749,20 @@ jQuery(document).ready(function() {
         jQuery('ul.navigationMenu').css('left', '0px');
     });
 });
+$(document).ready(function() {
+    $( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+})
