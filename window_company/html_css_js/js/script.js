aa3904b37661acc0c6c48fbe34c800594c6d383b
Olha Hryhorenko <olha.hryhorenko@i.ua>
2016-06-22 13:43:18 +0300

    Add files via upload

--- window_company/html_css_js/js/script.js
+++ window_company/html_css_js/js/script.js
@@ -1,8 +1,3 @@
-    lightbox.option({
-      'resizeDuration': 200,
-      'wrapAround': true
-    })
-
 
 /*slider*/
 jQuery(document).ready(function ($) {
@@ -51,4 +46,124 @@ jQuery(document).ready(function ($) {
     });
 
 });    
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+  auto: true,
+   minSlides: 2,
+  maxSlides: 2,
+  slideWidth: 470,
+  slideMargin: 20,
+  pager:false
 
+  });
+});
+
+/*modal_first*/
+$(document).ready(function() { // вся мaгия пoсле зaгрузки стрaницы
+    $('.callback_btn, .callback_footer_btn').click( function(event){ // лoвим клик пo ссылки с id="go"
+        event.preventDefault(); // выключaем стaндaртную рoль элементa
+        $('#overlay').fadeIn(400, // снaчaлa плaвнo пoкaзывaем темную пoдлoжку
+            function(){ // пoсле выпoлнения предъидущей aнимaции
+                $('#modal_form') 
+                    .css('display', 'block') // убирaем у мoдaльнoгo oкнa display: none;
+                    .animate({opacity: 1, top: '50%'}, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+        });
+    });
+    /* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
+    $('#modal_close, #overlay').click( function(){ // лoвим клик пo крестику или пoдлoжке
+        $('#modal_form')
+            .animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+                function(){ // пoсле aнимaции
+                    $(this).css('display', 'none'); // делaем ему display: none;
+                    $('#overlay').fadeOut(400); // скрывaем пoдлoжку
+                }
+            );
+    });
+});
+/*modal_second*/
+
+$(document).ready(function() { // вся мaгия пoсле зaгрузки стрaницы
+    $('.more_btn').click( function(event){ // лoвим клик пo ссылки с id="go"
+        event.preventDefault(); // выключaем стaндaртную рoль элементa
+        $('#overlay1').fadeIn(400, // снaчaлa плaвнo пoкaзывaем темную пoдлoжку
+            function(){ // пoсле выпoлнения предъидущей aнимaции
+                $('#modal_form1') 
+                    .css('display', 'block') // убирaем у мoдaльнoгo oкнa display: none;
+                    .animate({opacity: 1, top: '50%'}, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+        });
+    });
+    /* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
+    $('#modal_close1, #overlay1').click( function(){ // лoвим клик пo крестику или пoдлoжке
+        $('#modal_form1')
+            .animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+                function(){ // пoсле aнимaции
+                    $(this).css('display', 'none'); // делaем ему display: none;
+                    $('#overlay1').fadeOut(400); // скрывaем пoдлoжку
+                }
+            );
+    });
+});
+
+/*navigation*/
+$(".first").click(function(event) {
+    event.preventDefault();
+    $('html,body').animate({
+        scrollTop: $(".production").offset().top},
+        'slow');
+});
+$(".second").click(function(event) {
+    event.preventDefault()
+    $('html,body').animate({
+        scrollTop: $(".why_we").offset().top},
+        'slow');
+});
+$(".third").click(function(event) {
+    event.preventDefault()
+    $('html,body').animate({
+        scrollTop: $(".review").offset().top},
+        'slow');
+});
+$(".fourth").click(function(event) {
+    event.preventDefault()
+    $('html,body').animate({
+        scrollTop: $(".payment").offset().top},
+        'slow');
+});
+
+
+lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true
+    })
+/*carousel*/
+ var list = jQuery('.item_slider').find('.item_slider_list');                /*список слайдов*/
+    var items = jQuery(".item_slider_list").find('.carusel_item');              /*один слайд*/
+    var slides = jQuery('.item_slider_list').children('.carusel_item');         /*количество слайдов*/
+    var slide_width = items.outerWidth() + 30;                                      /*длина одного слайда*/
+    var carusel_width = slide_width + 30;
+
+    items.each(function() {
+        carusel_width += jQuery(this).outerWidth();
+    });
+
+    jQuery('.item_slider_list').css('width', carusel_width+'px');
+
+    var pixelsOffset = slide_width;
+    var currentLeftValue = 0;
+    var elementsCount = jQuery('.item_slider_list').find('.carusel_item').length;
+    var minimumOffset = - ((elementsCount - 3) * pixelsOffset);
+    var maximumOffset = 0;
+
+    jQuery(".arrow_left").click(function() {
+        if (currentLeftValue != maximumOffset) {
+            currentLeftValue += slide_width;
+            jQuery('.item_slider_list').animate({ left : currentLeftValue + "px"}, 300);
+        }
+    });
+
+    jQuery(".arrow_right").click(function() {
+        if (currentLeftValue != minimumOffset) {
+            currentLeftValue -= slide_width;
+            jQuery('.item_slider_list').animate({ left : currentLeftValue + "px"}, 300);
+        }
+    });
