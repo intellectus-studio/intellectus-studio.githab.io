a147c6fb7c19ff5869c12117d5b751a91c4c1f35
Olha Hryhorenko <olha.hryhorenko@i.ua>
2016-07-19 12:04:02 +0300

    Add files via upload

---
+++ halal/js/script.js
@@ -0,0 +1,91 @@
+jQuery('.bxslider').bxSlider({
+  auto: true,
+  pause: 10000,
+  autoControls: true
+});
+
+jQuery(document).ready(function () {
+  
+    jQuery("#partners").owlCarousel({
+            autoPlay: 3000,
+            stopOnHover: true,
+            navigation: true,
+            paginationSpeed: 1000,
+            goToFirstSpeed: 3500,
+            singleItem: false,
+            transitionStyle: "fade",
+                    navigationText: [
+                    "<i class='fa fa-angle-left'></i>",
+                    "<i class='fa fa-angle-right'></i>"
+            ]
+        });	
+  
+});
+
+
+/*callback*/
+jQuery(document).ready(function () {
+     jQuery('#modal_form_recall').removeClass('recall_toggle')
+ });
+
+ jQuery('.callback_btn').on('click', function () {
+     jQuery('#modal_form_recall').toggleClass('recall_toggle')
+         jQuery('#modal_form_recall').addClass('slideInDown');
+ });
+
+ jQuery(document).mouseup(function (e) {
+     var popup = jQuery("#modal_form_recall");
+     if (!jQuery('.callback_btn').is(e.target) && !popup.is(e.target) && popup.has(e.target).length == 0) {
+         popup.removeClass('recall_toggle');
+     }
+ });
+
+ jQuery(document).ready(function() {
+    function initialize()
+{
+    var latlng = new google.maps.LatLng(50.465668, 30.499538);
+    var latlng2 = new google.maps.LatLng(50.465668, 30.499538);
+    var imagePath = 'http://m.schuepfen.ch/icons/helveticons/black/60/Pin-location.png'
+    var myOptions =
+    {
+        zoom: 15,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var myOptions2 =
+    {
+        zoom: 15,
+        center: latlng2,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    
+    var map2 = new google.maps.Map(document.getElementById("map_canvas_2"), myOptions2);
+var goldStar = {
+    path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+    fillColor: 'yellow',
+    fillOpacity: 0.8,
+    scale: 1,
+    strokeColor: 'gold',
+    strokeWeight: 14
+  };
+
+    var myMarker = new google.maps.Marker(
+    {
+        position: latlng,
+        map: map,
+        title:"Pune",
+        icon: imagePath
+   });
+
+    var myMarker2 = new google.maps.Marker(
+    {
+        position: latlng2,
+        map: map2,
+        icon: imagePath
+    });
+}
+initialize();
+ })
