f6b2590e1be297985c42bbf2b058cc7fc73c02bc
Intellectus-studio <maxim.chzhen@gmail.com>
2016-10-17 00:00:51 +0300

    firstTry

--- Domolend/js/js.js
+++ Domolend/js/js.js
@@ -24,7 +24,9 @@ jQuery(document).ready(function () {
     });
 
     var timer = setInterval(nSlide, 5000);
-
+    if($(window).width()<=425){
+        clearInterval(timer);
+    }
     $('#lArrow').click(function () {
         var currentSlide = $('.active-slide');
         var prevSlide = currentSlide.prev();
@@ -44,41 +46,86 @@ jQuery(document).ready(function () {
          prevDot.addClass('active-dot');*/
     });
 
+
+    $('.corpBurger').click(function(){
+        if ($('.corpMenu').css('display') == 'none') {
+            $('.corpMenu').fadeIn(500);
+        } else {
+            $('.corpMenu').fadeOut(500);
+        }
+    })
+
+
+    /*mobile filter*/
+    $('.bx-filter-parameters-box-title').click(function () {
+        if( $(this).next().css('display') == 'none'){
+        $(this).next().slideDown(300);}else{
+            $(this).next().slideUp(300);
+        }
+    })
+
+
     /*товары-слайдер*/
+    if ($('.goods_sliders').length > 0) {
+        var width = 250;
+        var count = 4;
+        var carousel = $('.goods_sliders')[0];
+        var list = $('.goods_float_left')[0];
+        var listItems = carousel.querySelectorAll('.products');
+        var positionLeft = 0;
 
-    var width = 250;
-    var count = 4;
-    var carousel = $('.goods_sliders')[0];
-    var list = $('.goods_float_left')[0];
-    var listItems = carousel.querySelectorAll('.products');
-    var positionLeft = 0;
+        $('#goodsLeft')[0].onclick = function () {
+            positionLeft = Math.min(positionLeft + width * count, 0);
+            list.style.marginLeft = positionLeft + 'px';
+        }
 
-    $('#goodsLeft')[0].onclick = function () {
-        positionLeft = Math.min(positionLeft + width * count, 0);
-        list.style.marginLeft = positionLeft + 'px';
+        $('#goodsRight')[0].onclick = function () {
+            positionLeft = Math.max(positionLeft - width * count, -width * (listItems.length - count));
+            list.style.marginLeft = positionLeft + 'px';
+        }
+    }
+    /*1slider*/
+
+    if ($('.goods_sliders1').length > 0) {
+        var width2 = 250;
+        var count2 = 4;
+        var carousel2 = $('.goods_sliders1')[0];
+        var list2 = $('.goods_float_left1')[0];
+        var listItems2 = carousel2.querySelectorAll('.products');
+        var positionLeft2 = 0;
+
+        $('#goodsLeft1')[0].onclick = function () {
+            positionLeft2 = Math.min(positionLeft2 + width2 * count2, 0);
+            list2.style.marginLeft = positionLeft2 + 'px';
+        }
+
+        $('#goodsRight1')[0].onclick = function () {
+            positionLeft2 = Math.max(positionLeft2 - width2 * count2, -width2 * (listItems2.length - count2));
+            list2.style.marginLeft = positionLeft2 + 'px';
+        }
     }
 
-    $('#goodsRight')[0].onclick = function () {
-        positionLeft = Math.max(positionLeft - width * count, -width * (listItems.length - count));
-        list.style.marginLeft = positionLeft + 'px';
+    if ($('.goods_sliders2').length > 0) {
+        var width3 = 250;
+        var count3 = 4;
+        var carousel3 = $('.goods_sliders2')[0];
+        var list3 = $('.goods_float_left2')[0];
+        var listItems3 = carousel3.querySelectorAll('.products');
+        var positionLeft3 = 0;
+
+        $('#goodsLeft2')[0].onclick = function () {
+            positionLeft3 = Math.min(positionLeft3 + width3 * count3, 0);
+            list3.style.marginLeft = positionLeft3 + 'px';
+        }
+
+        $('#goodsRight2')[0].onclick = function () {
+            positionLeft3 = Math.max(positionLeft3 - width3 * count3, -width3 * (listItems3.length - count3));
+            list3.style.marginLeft = positionLeft3 + 'px';
+        }
     }
-
-    /*?????? ??????*/
-
-    $('.fa-chevron-down').mouseover(function () {
-        $('.phones').animate({
-            opacity: 1
-        }, 200);
-
-        $('.fa-chevron-down').mouseleave(function () {
-            $('.phones').animate({opacity: 0}, 200);
-        });
-
-    });
-
 
     /*слайдер бренды*/
-    if ($('.bSlider')) {
+    if ($('.bSlider').length) {
         var width1 = 170.4;
         var count1 = 6;
         var carousel1 = $('.bSlider')[0];
@@ -96,6 +143,8 @@ jQuery(document).ready(function () {
             list1.style.marginLeft = positionLeft1 + 'px';
         }
     }
+
+
     /*pop ???? ??? ????????? ?????? ? ??????*/
 
     $('#modal_form_recall, #modal_form_recall1').removeClass('recall_toggle');
@@ -119,11 +168,11 @@ jQuery(document).ready(function () {
         $('#submit1, #overlay').click(function () {
             $('#modal_form_recall1')
                 .animate({opacity: 0, top: '45%'}, 200,
-                function () {
-                    $(this).css('display', 'none');
-                    $('#overlay').fadeOut(400);
-                }
-            );
+                    function () {
+                        $(this).css('display', 'none');
+                        $('#overlay').fadeOut(400);
+                    }
+                );
         });
     });
 
@@ -185,52 +234,417 @@ jQuery(document).ready(function () {
 
     });
 
+
+    $('.tab').click(function () {
+        var active = $('.activePageType');
+        active.removeClass('activePageType');
+        $('div.products').removeClass('products').addClass('productsProduct');
+        $(this).addClass('activePageType');
+
+    })
+
+    $('.list').click(function () {
+        var active = $('.activePageType');
+        active.removeClass('activePageType');
+        $('div.productsProduct').removeClass('productsProduct').addClass('products');
+        $(this).addClass('activePageType');
+
+    })
+
+    // go to top button
+
+    if ($(document).scrollTop() > 1000) {
+        $('.lift').css({'opacity': '1', 'cursore': 'pointer'});
+    }
+
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 1000) {
+            $('.lift').css({'opacity': '1', 'cursor': 'pointer','visibility': 'visible'});
+        }
+
+        else {
+            $('.lift').css({'opacity': '0', 'cursor': 'default','visibility': 'hidden'});
+        }
+    });
+
+    $('.lift').on('click', function () {
+        if ($(document).scrollTop() < 1000) {
+            return;
+        }
+
+        $('html,body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    /*popUp logIn*/
+    $('.clPopUp').click(function () {
+        var active = $('.active-clientsPopUp');
+        var ac = $('.active-clientPopUp');
+        var num = $(this).index();
+
+        if ($(this).hasClass('active-clientsPopUp')) return;
+
+        active.removeClass('active-clientsPopUp');
+        ac.hide(300).removeClass('active-clientPopUp');
+
+        var next = $('.clientPopUp').eq(num);
+
+        $(this).addClass('active-clientsPopUp');
+        next.show(300).addClass('active-clientPopUp');
+    });
+
+    $('.forget').click(function () {
+        $('.hide').hide();
+        $('.show').show();
+    })
+
+
+    $('.login a').click(function () {
+        $('#overlay').fadeIn(400, function () {
+            $('.popPrivatRoom').css('display', 'block')
+                .animate({opacity: 1, top: '50%'}, 300);
+        })
+    })
+
+
+    $('#overlay').click(function () {
+        $('.popPrivatRoom')
+            .animate({opacity: 0, top: '45%'}, 200,
+                function () {
+                    $('.popPrivatRoom').css('display', 'none');
+                    $('#overlay').css('display', 'none').fadeOut(400);
+                }
+            );
+    });
+
+    $('.tellWhen').click(function () {
+        $('#overlay').fadeIn(400, function () {
+            $('.when').css('display', 'block')
+                .animate({opacity: 1, top: '50%'}, 300);
+        })
+    })
+
+
+    $('#overlay').click(function () {
+        $('.when')
+            .animate({opacity: 0, top: '45%'}, 200,
+                function () {
+                    $('.when').css('display', 'none');
+                    $('#overlay').css('display', 'none').fadeOut(400);
+                }
+            );
+    });
+
+
+    $('.buy').click(function (event) {
+        event.preventDefault();
+        $('#overlay').fadeIn(400,
+            function () {
+                $('#modal_form')
+                    .css('display', 'block')
+                    .animate({opacity: 1, top: '30%'}, 200);
+            });
+    });
+
+    $('#modal_close, #overlay').click(function () {
+        $('#modal_form')
+            .animate({opacity: 0, top: '45%'}, 200,
+                function () {
+                    $(this).css('display', 'none');
+                    $('#overlay').fadeOut(400);
+                }
+            );
+    });
+
+
+    /*Bucket Img*/
+
+    $(".ImgSlider img").click(function () {
+
+        $(this)
+
+        var largePath = $(this).attr("src");
+        var largeAlt = $(this).attr("alt");
+
+        $("#big_img").attr({src: largePath, alt: largeAlt});
+
+    });
+
+    /*delete goods*/
+
+    if ($('.removeTov')) {
+        $('.removeTov').click(function () {
+            $(this).closest('.goodsTov').animate({opacity: 0}, 500, function () {
+                $(this).remove()
+            });
+
+
+        });
+    }
+
+    if ($('.remove')) {
+        $('.remove').click(function () {
+            $(this).closest('tr').animate({opacity: 0}, 500, function () {
+                $(this).remove()
+            });
+        });
+    }
+
+    var typeOfPayment = 0; //0 - paper money/ 1 - web money
+
+    $('.payment').click(function () {
+        var active = $('.active-payment');
+        if ($(this).hasClass('active-payment')) return;
+        num = $(this).index();
+
+        switch (num) {
+            case 1:
+                typeOfPayment = 1;
+                break;
+            case 2:
+                typeOfPayment = 0;
+                break
+        }
+
+
+        console.log(typeOfPayment);
+        active.removeClass('active-payment');
+        $(this).addClass('active-payment');
+    });
+
+    $('.cl').click(function () {
+        var active = $('.active-clients');
+        var ac = $('.active-client');
+        var num = $(this).index();
+
+        if ($(this).hasClass('active-clients')) return;
+
+        active.removeClass('active-clients');
+        ac.removeClass('active-client');
+
+        var next = $('.client').eq(num);
+
+        $(this).addClass('active-clients');
+        next.addClass('active-client');
+    });
+
+    $('.pDescriptionButt').click(function () {
+        var active = $('.active-pDescriptionButt');
+        var ac = $('.active-pDescriptionContent');
+        var num = $(this).index();
+
+        if ($(this).hasClass('active-pDescriptionButt')) return;
+
+        active.removeClass('active-pDescriptionButt');
+        ac.slideUp(300).removeClass('active-pDescriptionContent');
+
+        var next = $('.pDescriptionContent ').eq(num);
+
+        $(this).addClass('active-pDescriptionButt');
+        next.slideDown(300).addClass('active-pDescriptionContent');
+    });
+
+    $('.delivery').click(function () {
+        var active = $('.active-delivery');
+        var ac = $('.active-deliveryType');
+        var num = $(this).index();
+        if ($(this).hasClass('active-delivery')) return;
+
+        switch (num) {
+            case 1:
+                num = 2;
+                break;
+            case 2:
+                num = 1;
+                break;
+            case 3:
+                num = 0;
+                break;
+        }
+
+        active.removeClass('active-delivery');
+        ac.removeClass('active-deliveryType');
+
+        var next = $('.deliveryType').eq(num);
+
+        console.log(num);
+        console.log(next.index());
+
+        $(this).addClass('active-delivery');
+        next.addClass('active-deliveryType');
+    })
+
+    $('.moveWithOutRegistration').click(function (e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top - 207;
+        $('body,html').animate({scrollTop: top}, 1500);
+
+    })
+
+    if ($('.showNews').length > 0) {
+        $('.showNews').click(function () {
+            $('.newses').slideDown(500);
+        })
+    }
+
+
+    /*Img Box*/
+    $('#big_img').click(function (e) {
+        var largePath1 = $(this).attr("src");
+        var largeAlt1 = $(this).attr("alt");
+
+        $("#largeImgPop").attr({src: largePath1, alt: largeAlt1});
+        $('#overlay').fadeIn(400, function () {
+            $('.pop_up1').css('display', 'block')
+                .animate({opacity: 1, top: '50%'}, 300);
+        })
+    })
+
+
+    $('#overlay').click(function () {
+        $('.pop_up1')
+            .animate({opacity: 0, top: '45%'}, 200,
+                function () {
+                    $('#popSlide').css('margin-left', 0);
+                    $('.pop_up1').css('display', 'none');
+                    $('#overlay').css('display', 'none').fadeOut(400);
+                }
+            );
+    });
+
+
+    if ($('.pop_up1_position').length > 0) {
+        var width = 400;
+
+
+
+        var count = 1;
+        var carousel = $('.pop_up1_position')[0];
+        var list = $('#popSlide')[0];
+        var listItems = list.querySelectorAll('.fade');
+        var positionLeft = 0;
+
+        $('.prevPopArrow')[0].onclick = function () {
+            var wid = $(window).width();
+            if(wid<=425){
+                width=300;
+            }
+            positionLeft = Math.min(positionLeft + width * count, 0);
+            list.style.marginLeft = positionLeft + 'px';
+        }
+
+        $('.nextPopArrow')[0].onclick = function () {
+            var wid = $(window).width();
+            if(wid<=425){
+                width=300;
+            }
+            positionLeft = Math.max(positionLeft - width * count, -width * (listItems.length - count));
+            list.style.marginLeft = positionLeft + 'px';
+        }
+    }
+    /*fixed scroll*/
+
+    var borderBottom = document.querySelector('.borderBottom');
+    borderBottom = borderBottom.offsetHeight + parseInt(getComputedStyle(borderBottom).marginBottom);
+
+    var topHeight = 297;
+    if($(window).width()== 768){
+        topHeight = 367;
+    }
+
+    $(window).resize(function(){
+        if($(window).width()== 768){
+            topHeight = 367;
+        }
+        else if($(window).width()>= 1024){
+            topHeight = 297;
+        }
+    })
+
+    $('.menuTop').css('height', topHeight);
+
+
+    var h_hght = borderBottom;
+    var h_mrg = 0;
+
+    var elem = $('.fixedMenu');
+    var elem2 = $('.menu');
+    var top = $(this).scrollTop();
+
+    if (top > h_hght) {
+        elem.css('top', h_mrg);
+        elem2.animate({marginTop: '0px'}, 1000);
+    }
+    $(window).scroll(function () {
+        top = $(this).scrollTop();
+
+        if (top + h_mrg < h_hght) {
+            elem.css('top', (h_hght - top));
+            elem2.css('marginTop', 20);
+        } else {
+            elem.css('top', h_mrg);
+            elem2.css('marginTop', 0);
+        }
+    });
+
+
+    $('.burger').click(function () {
+        if ($('.menu').css('display') == 'none') {
+            $('.menu').fadeIn(500);
+        } else {
+            $('.menu').fadeOut(500);
+        }
+    })
+
 });
 
-jQuery.fn.sortElements = (function () {
+/*
+ jQuery.fn.sortElements = (function () {
 
-    var sort = [].sort;
+ var sort = [].sort;
 
-    return function (comparator, getSortable) {
+ return function (comparator, getSortable) {
 
-        getSortable = getSortable ||
-            function () {
-                return this;
-            };
+ getSortable = getSortable ||
+ function () {
+ return this;
+ };
 
-        var placements = this.map(function () {
+ var placements = this.map(function () {
 
-            var sortElement = getSortable.call(this),
-                parentNode = sortElement.parentNode,
+ var sortElement = getSortable.call(this),
+ parentNode = sortElement.parentNode,
 
-            // Since the element itself will change position, we have
-            // to have some way of storing its original position in
-            // the DOM. The easiest way is to have a 'flag' node:
-                nextSibling = parentNode.insertBefore(
-                    document.createTextNode(''), sortElement.nextSibling);
+ // Since the element itself will change position, we have
+ // to have some way of storing its original position in
+ // the DOM. The easiest way is to have a 'flag' node:
+ nextSibling = parentNode.insertBefore(
+ document.createTextNode(''), sortElement.nextSibling);
 
-            return function () {
+ return function () {
 
-                if (parentNode === this) {
-                    throw new Error("You can't sort elements if any one is a descendant of another.");
-                }
+ if (parentNode === this) {
+ throw new Error("You can't sort elements if any one is a descendant of another.");
+ }
 
-                // Insert before flag:
-                parentNode.insertBefore(this, nextSibling);
-                // Remove flag:
-                parentNode.removeChild(nextSibling);
+ // Insert before flag:
+ parentNode.insertBefore(this, nextSibling);
+ // Remove flag:
+ parentNode.removeChild(nextSibling);
 
-            };
+ };
 
-        });
+ });
 
-        return sort.call(this, comparator).each(function (i) {
-            placements[i].call(getSortable.call(this));
-        });
+ return sort.call(this, comparator).each(function (i) {
+ placements[i].call(getSortable.call(this));
+ });
 
-    };
+ };
 
-})();
+ })();*/
 
 /*
  $(document).mouseup(function (e) {
